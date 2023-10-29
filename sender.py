d4800a6e17e01629ce038f85baf1a7205c6d79d5
alizadeht <turalalizadeh4@gmail.com>
2023-10-29 22:11:34 +0400

    function for manually entering database details

--- sender.py
+++ sender.py
@@ -1,21 +1,27 @@
 import threading
 import time
 import psycopg2
+import random
+import json
 
-# Function for inserting messages -> to the database
-def insert_message(conn, sender_name, message):
-    current_time = time.strftime('%Y-%m-%d %H:%M:%S')
+# Function to insert a message into the database
+def insert_message(message, sender_name, conn):
+    sent_time = time.strftime('%Y-%m-%d %H:%M:%S')
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO async_messages (sender_name, message, sent_time) VALUES (%s, %s, %s)", (sender_name, message, current_time))
+    cursor.execute("INSERT INTO async_messages (sender_name, message, sent_time) VALUES (%s, %s, %s)", (sender_name, message, sent_time))
     conn.commit()
-    cursor.close()
 
-# Function to send messages
-def sender_thread(db_server, sender_name):
-    conn = psycopg2.connect(db_server)  # Connect to the database
-    while True:
-        message = input("Enter your message (or type 'exit' to quit): ")
-        if message.lower() == 'exit':
-            break
-        insert_message(conn, sender_name, message)
-    conn.close()
+# Prompt the user for database details
+db_name = input("Enter the database name: ")
+db_user = input("Enter the database user: ")
+db_password = input("Enter the database password: ")
+
+# Store the details of the database in a configuration file
+config = {
+    "db_name": db_name,
+    "db_user": db_user,
+    "db_password": db_password
+}
+
+with open('config.json', 'w') as config_file:
+    json.dump(config, config_file)
